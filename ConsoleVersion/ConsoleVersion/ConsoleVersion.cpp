6ecf12197ec096039d7e55c7ebec00f428c4e216
keshe4ka <keshe4ka@users.noreply.github.com>
2020-03-10 22:17:15 +0300

    консольная версия

--- ConsoleVersion/ConsoleVersion/ConsoleVersion.cpp
+++ ConsoleVersion/ConsoleVersion/ConsoleVersion.cpp
@@ -3,23 +3,7 @@
 #include "administrator.h"
 using namespace std;
 
-//функция стартового окна, которая предлагает пользователю войти или зарегистрироваться
-void start_window() {
-	cout << "Write 'S' if you want sign in \n or write 'U' for sign up" << endl;
-	char input_value;
-	cin >> input_value;
-	if (input_value == 'S') {
-		sign_in();
-	}
-	else if (input_value == 'U') {
-		sign_up();
-	}
-	//
-	else {
-		cout << "Uncorrect symbol" << endl;
-		start_window();
-	}
-}
+
 
 //функция входа
 void sign_in() {
@@ -32,7 +16,7 @@ void sign_in() {
 }
 
 //функция регистрации
-void sign_in() {
+void sign_up() {
 	cout << "login: " << endl;
 	string login;
 	cin >> login;
@@ -41,7 +25,26 @@ void sign_in() {
 	cin >> password;
 }
 
+//функция стартового окна, которая предлагает пользователю войти или зарегистрироваться
+void start_window() {
+	cout << "Write 'S' if you want sign in \n or write 'U' for sign up" << endl;
+	char input_value;
+	cin >> input_value;
+	if ((input_value == 'S') || (input_value == 's') ) {
+		sign_in();
+	}
+	else if (input_value == 'U' || (input_value == 'u')) {
+		sign_up();
+	}
+	//защита от дебила
+	else {
+		cout << "Uncorrect symbol" << endl;
+		start_window();
+	}
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	start_window();
+	return 0;
 }
