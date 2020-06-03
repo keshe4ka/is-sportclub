95796459cc5550e8b116385462e009680ab6230d
keshe4ka <keshe4ka@users.noreply.github.com>
2020-06-03 04:00:12 +0300

    получилось ебать!

---
+++ winforms/winforms/DB.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winforms
+{
+    class DB
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost; port=3306; username=root; password=root; database=mydb");
+
+        //открываем соединение с бд
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+
+        //закрываем
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        //возвращаем но нахуя не понял
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
