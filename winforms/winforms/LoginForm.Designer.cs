fe1d8d82b09c154bd9e37af7bfeec0a69ade1a8c
keshe4ka <keshe4ka@users.noreply.github.com>
2020-06-04 01:40:03 +0300

    backup

---
+++ winforms/winforms/LoginForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace winforms
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.login_lable = new System.Windows.Forms.Label();
+            this.password_label = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.login_textbox = new System.Windows.Forms.TextBox();
+            this.password_textbox = new System.Windows.Forms.TextBox();
+            this.login_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // login_lable
+            // 
+            this.login_lable.AutoSize = true;
+            this.login_lable.Location = new System.Drawing.Point(150, 337);
+            this.login_lable.Name = "login_lable";
+            this.login_lable.Size = new System.Drawing.Size(51, 17);
+            this.login_lable.TabIndex = 1;
+            this.login_lable.Text = "Логин:";
+            // 
+            // password_label
+            // 
+            this.password_label.AutoSize = true;
+            this.password_label.Location = new System.Drawing.Point(140, 383);
+            this.password_label.Name = "password_label";
+            this.password_label.Size = new System.Drawing.Size(61, 17);
+            this.password_label.TabIndex = 2;
+            this.password_label.Text = "Пароль:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(555, 304);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // login_textbox
+            // 
+            this.login_textbox.Location = new System.Drawing.Point(207, 337);
+            this.login_textbox.Name = "login_textbox";
+            this.login_textbox.Size = new System.Drawing.Size(189, 22);
+            this.login_textbox.TabIndex = 4;
+            // 
+            // password_textbox
+            // 
+            this.password_textbox.Location = new System.Drawing.Point(207, 380);
+            this.password_textbox.Name = "password_textbox";
+            this.password_textbox.Size = new System.Drawing.Size(189, 22);
+            this.password_textbox.TabIndex = 5;
+            this.password_textbox.UseSystemPasswordChar = true;
+            // 
+            // login_button
+            // 
+            this.login_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.login_button.Location = new System.Drawing.Point(207, 423);
+            this.login_button.Name = "login_button";
+            this.login_button.Size = new System.Drawing.Size(189, 32);
+            this.login_button.TabIndex = 6;
+            this.login_button.Text = "Войти";
+            this.login_button.UseVisualStyleBackColor = true;
+            this.login_button.Click += new System.EventHandler(this.login_button_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(579, 477);
+            this.Controls.Add(this.login_button);
+            this.Controls.Add(this.password_textbox);
+            this.Controls.Add(this.login_textbox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.password_label);
+            this.Controls.Add(this.login_lable);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label login_lable;
+        private System.Windows.Forms.Label password_label;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox login_textbox;
+        private System.Windows.Forms.TextBox password_textbox;
+        private System.Windows.Forms.Button login_button;
+    }
+}
---
+++ winforms/winforms/LoginForm.cs
@@ -0,0 +1,53 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winforms
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void login_button_Click(object sender, EventArgs e)
+        {
+            //Создаем строки, которые заполняем вводом из текстбоксов
+            String loginUser = login_textbox.Text;
+            String passUser = password_textbox.Text;
+
+            //Делаем подключение к базе данных
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            //@loginUser и @loginPassword - заглушки, которые нужны для защиты от хацкеров, далее мы передаем этим заглушкам созданные строки
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `user` WHERE `login` = @loginUser AND `password` = @passUser", db.getConnection()); 
+            command.Parameters.Add("@loginUser", MySqlDbType.VarChar).Value = loginUser;
+            command.Parameters.Add("@passUser", MySqlDbType.VarChar).Value = passUser;
+
+            //Выбрали нужную комманду и выполнили ее
+            adapter.SelectCommand = command;
+            //Все полученные данные поместили в созданную табличку
+            adapter.Fill(table);
+
+            //Если записи есть, то...
+            if (table.Rows.Count > 0)
+                MessageBox.Show("Вы вошли!");
+            else
+                MessageBox.Show("Не вошли :(");
+            
+
+        }
+    }
+}
